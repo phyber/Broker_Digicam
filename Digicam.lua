5f4427c3b4df647eb97395978a0e780b09951837
phyber <phyber@localhost>
2016-12-31 17:15:17 +0000

    Convert tabs to spaces

--- Digicam.lua
+++ Digicam.lua
@@ -4,9 +4,9 @@ local LDB = LibStub:GetLibrary("LibDataBroker-1.1")
 local LQT = LibStub("LibQTip-1.0")
 local L = LibStub("AceLocale-3.0"):GetLocale("Broker_Digicam")
 local dataobj = LDB:NewDataObject("Broker_Digicam", {
-	type = "data source",
-	text = "Digicam",
-	icon = "Interface\\Icons\\INV_Misc_Spyglass_03",
+    type = "data source",
+    text = "Digicam",
+    icon = "Interface\\Icons\\INV_Misc_Spyglass_03",
 })
 local tonumber = tonumber
 local Screenshot = Screenshot
@@ -18,119 +18,119 @@ local SetCVar = SetCVar
 local NORMAL_FONT_COLOR_CODE = NORMAL_FONT_COLOR_CODE
 
 function Broker_Digicam:SetFormat(ssFormat)
-	-- Possible formats: tga, jpeg, png
-	-- PNG is currently Mac only.
-	-- Cycle order: tga -> jpeg -> png
-	local newFormat
-	
-	if ssFormat == "tga" then
-		newFormat = "jpeg"
-	end
+    -- Possible formats: tga, jpeg, png
+    -- PNG is currently Mac only.
+    -- Cycle order: tga -> jpeg -> png
+    local newFormat
 
-	if ssFormat == "jpeg" then
-		if IsMacClient() then
-			newFormat = "png"
-		else
-			newFormat = "tga"
-		end
-	end
+    if ssFormat == "tga" then
+        newFormat = "jpeg"
+    end
 
-	if ssFormat == "png" then
-		newFormat = "tga"
-	end
+    if ssFormat == "jpeg" then
+        if IsMacClient() then
+            newFormat = "png"
+        else
+            newFormat = "tga"
+        end
+    end
 
-	if newFormat then
-		SetCVar("screenshotFormat", newFormat)
-	end 
-	Broker_Digicam:DrawTooltip()
+    if ssFormat == "png" then
+        newFormat = "tga"
+    end
+
+    if newFormat then
+        SetCVar("screenshotFormat", newFormat)
+    end
+    Broker_Digicam:DrawTooltip()
 end
 
 function Broker_Digicam:SetQuality(ssQuality)
-	ssQuality = tonumber(ssQuality)
-	-- Holt down Alt key to decrease quality
-	if GetMouseButtonClicked() == "RightButton" then
-		if ssQuality > 0 then
-			ssQuality = ssQuality - 1
-			SetCVar("screenshotQuality", ssQuality)
-		end
-	else -- Increase quality
-		if ssQuality < 10 then
-			ssQuality = ssQuality + 1
-			SetCVar("screenshotQuality", ssQuality)
-		end
-	end
-	Broker_Digicam:DrawTooltip()
+    ssQuality = tonumber(ssQuality)
+    -- Holt down Alt key to decrease quality
+    if GetMouseButtonClicked() == "RightButton" then
+        if ssQuality > 0 then
+            ssQuality = ssQuality - 1
+            SetCVar("screenshotQuality", ssQuality)
+        end
+    else -- Increase quality
+        if ssQuality < 10 then
+            ssQuality = ssQuality + 1
+            SetCVar("screenshotQuality", ssQuality)
+        end
+    end
+    Broker_Digicam:DrawTooltip()
 end
 
 local tooltip
 function Broker_Digicam:HideTooltip()
-	if MouseIsOver(tooltip) then
-		return
-	end
-	tooltip:Hide()
-	LQT:Release(tooltip)
-	tooltip = nil
+    if MouseIsOver(tooltip) then
+        return
+    end
+    tooltip:Hide()
+    LQT:Release(tooltip)
+    tooltip = nil
 end
 
 function Broker_Digicam:DrawTooltip()
-	tooltip:Hide()
-	tooltip:Clear()
-	local linenum
-	local ssFormat = GetCVar("screenshotFormat")
-	local ssQuality = GetCVar("screenshotQuality")
-	local NFC = NORMAL_FONT_COLOR
-	-- Header
-	tooltip:AddLine(("%s%s|r"):format(NORMAL_FONT_COLOR_CODE, GetAddOnMetadata("Broker_Digicam", "Title")))
-	tooltip:AddLine(" ", " ")
+    tooltip:Hide()
+    tooltip:Clear()
+    local linenum
+    local ssFormat = GetCVar("screenshotFormat")
+    local ssQuality = GetCVar("screenshotQuality")
+    local NFC = NORMAL_FONT_COLOR
+    -- Header
+    tooltip:AddLine(("%s%s|r"):format(NORMAL_FONT_COLOR_CODE, GetAddOnMetadata("Broker_Digicam", "Title")))
+    tooltip:AddLine(" ", " ")
 
-	-- Image Format
-	linenum = tooltip:AddLine(nil)
-	tooltip:SetCell(linenum, 1, ("%s%s|r"):format(NORMAL_FONT_COLOR_CODE, L["Image Format"]))
-	tooltip:SetCell(linenum, 2, ssFormat:upper())
-	tooltip:SetLineScript(linenum, "OnMouseUp", Broker_Digicam.SetFormat, ssFormat)
+    -- Image Format
+    linenum = tooltip:AddLine(nil)
+    tooltip:SetCell(linenum, 1, ("%s%s|r"):format(NORMAL_FONT_COLOR_CODE, L["Image Format"]))
+    tooltip:SetCell(linenum, 2, ssFormat:upper())
+    tooltip:SetLineScript(linenum, "OnMouseUp", Broker_Digicam.SetFormat, ssFormat)
 
-	-- Image Quality
-	linenum = tooltip:AddLine(nil)
-	tooltip:SetCell(linenum, 1, ("%s%s|r"):format(NORMAL_FONT_COLOR_CODE, L["Image Quality"]))
-	tooltip:SetCell(linenum, 2, ssQuality)
-	tooltip:SetLineScript(linenum, "OnMouseUp", Broker_Digicam.SetQuality, ssQuality)
+    -- Image Quality
+    linenum = tooltip:AddLine(nil)
+    tooltip:SetCell(linenum, 1, ("%s%s|r"):format(NORMAL_FONT_COLOR_CODE, L["Image Quality"]))
+    tooltip:SetCell(linenum, 2, ssQuality)
+    tooltip:SetLineScript(linenum, "OnMouseUp", Broker_Digicam.SetQuality, ssQuality)
 
-	-- Hints
-	tooltip:AddLine(" ", " ")
-	tooltip:AddLine(L["|cffffff00Shift-Click|r |cff00ff00broker icon to take a screenshot.|r"])
-	tooltip:AddLine(L["|cffffff00Left-Click|r |cff00ff00quality to increase.|r"])
-	tooltip:AddLine(L["|cffffff00Right-Click|r |cff00ff00quality to decrease.|r"])
+    -- Hints
+    tooltip:AddLine(" ", " ")
+    tooltip:AddLine(L["|cffffff00Shift-Click|r |cff00ff00broker icon to take a screenshot.|r"])
+    tooltip:AddLine(L["|cffffff00Left-Click|r |cff00ff00quality to increase.|r"])
+    tooltip:AddLine(L["|cffffff00Right-Click|r |cff00ff00quality to decrease.|r"])
 
-	-- Show it
-	-- Now we want to slightly override what SmartAnchorTo did
-	-- Since it will anchor to the corners, but we want the menu positioned
-	-- centrally over the icon, like other Broker doodads.
-	local p, rT, rP, x, y = tooltip:GetPoint()
-	tooltip:ClearAllPoints()
-	tooltip:SetPoint(p, rT, rP, -(tooltip:GetWidth()/2), y)
-	tooltip:Show()
+    -- Show it
+    -- Now we want to slightly override what SmartAnchorTo did
+    -- Since it will anchor to the corners, but we want the menu positioned
+    -- centrally over the icon, like other Broker doodads.
+    local p, rT, rP, x, y = tooltip:GetPoint()
+    tooltip:ClearAllPoints()
+    tooltip:SetPoint(p, rT, rP, -(tooltip:GetWidth()/2), y)
+    tooltip:Show()
 end
 
 function dataobj:OnEnter()
-	-- Tooltip Setup
-	if not LQT:IsAcquired("Broker_DigicamTip") then
-		tooltip = LQT:Acquire("Broker_DigicamTip", 2, "LEFT", "RIGHT")
-	end
-	tooltip:Clear()
-	tooltip:SmartAnchorTo(self)
-	tooltip:SetAutoHideDelay(0.25, self)
-	tooltip:SetScale(1)
+    -- Tooltip Setup
+    if not LQT:IsAcquired("Broker_DigicamTip") then
+        tooltip = LQT:Acquire("Broker_DigicamTip", 2, "LEFT", "RIGHT")
+    end
+    tooltip:Clear()
+    tooltip:SmartAnchorTo(self)
+    tooltip:SetAutoHideDelay(0.25, self)
+    tooltip:SetScale(1)
 
-	-- Draw it
-	Broker_Digicam:DrawTooltip()
+    -- Draw it
+    Broker_Digicam:DrawTooltip()
 end
 
 function dataobj:OnLeave()
-	Broker_Digicam:HideTooltip()
+    Broker_Digicam:HideTooltip()
 end
 
 function dataobj:OnClick(button)
-	if IsShiftKeyDown() and button == "LeftButton" then
-		Screenshot()
-	end
+    if IsShiftKeyDown() and button == "LeftButton" then
+        Screenshot()
+    end
 end
